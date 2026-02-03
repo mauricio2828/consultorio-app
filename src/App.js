7d383d87937a076803818f4689a9190c367c475c
Mauricio Ovando <mauricio_060792@hotmail.com>
2026-02-02 23:38:33 -0600

    Primer commit del consultorio

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: 30, fontFamily: 'Arial' }}>
+      <h1>Consultorio Médico</h1>
+
+      <p>Bienvenido al sistema del consultorio.</p>
+
+      <ul>
+        <li>📅 Citas médicas</li>
+        <li>👨‍⚕️ Pacientes</li>
+        <li>📄 Expedientes</li>
+        <li>💊 Tratamientos</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
